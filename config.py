62306437c9c0b25c9239f08aada22f2f8d0c07c3
devadethanr <125908755+devadethanr@users.noreply.github.com>
2024-09-07 13:17:52 +0530

    get mac addr, login changes

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/rescuenet'
+    MONGO_URI = os.environ.get('MONGO_URI')
